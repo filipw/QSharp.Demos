5b909f34438dd37ff95ee824441cbfda8285e8c5
filipw <wojcieszyn@gmail.com>
2020-04-07 11:08:11 +0200

    added qubit example

---
+++ QubitExample/Driver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace QubitExample
+{
+    class Driver
+    {
+        static async Task Main(string[] args)
+        {
+            using var qsim = new QuantumSimulator();
+            var repeats = 100;
+            Console.WriteLine($"Running qubit measurement {repeats} times.");
+
+            var results = await MeasureQubits.Run(qsim, repeats);
+            Console.WriteLine($"Received {results} ones.");
+            Console.WriteLine($"Received {repeats - results} zeros.");
+        }
+    }
+}
---
+++ QubitExample/Program.qs
@@ -0,0 +1,26 @@
+﻿namespace QubitExample {
+
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Measurement;
+    
+
+    operation MeasureQubits(count : Int) : Int {
+
+        mutable resultsTotal = 0;
+
+        using (qubit = Qubit()) {
+
+            for (idx in 0..count) {
+                
+                //let result = MResetZ(qubit);
+
+                let result = Measure([PauliX], [qubit]);
+                set resultsTotal += result == One ? 1 | 0;
+                Reset(qubit);
+            }
+
+            return resultsTotal;
+        }
+    }
+}
