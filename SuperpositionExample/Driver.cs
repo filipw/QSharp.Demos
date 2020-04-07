af75d9e3c021d0d8043db14e6e3dc42fef9c735f
filipw <wojcieszyn@gmail.com>
2020-04-07 21:43:28 +0200

    added superposition code

---
+++ SuperpositionExample/Driver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace QubitExample
+{
+    class Driver
+    {
+        static async Task Main(string[] args)
+        {
+            using var qsim = new QuantumSimulator();
+            var repeats = 1000;
+            Console.WriteLine($"Running qubit measurement {repeats} times.");
+
+            var results = await MeasureQubits.Run(qsim, repeats);
+            Console.WriteLine($"Received {results} ones.");
+            Console.WriteLine($"Received {repeats - results} zeros.");
+
+            var randomBits = await RandomNumberGenerator.Run(qsim);
+            var bitString = string.Join("", randomBits.Select(x => x ? 1 : 0));
+            Console.WriteLine($"Generated random bit string: {bitString}");
+            Console.WriteLine($"Generated random uint32: {Convert.ToUInt32(bitString, 2)}");
+        }
+    }
+}
---
+++ SuperpositionExample/Program.qs
@@ -0,0 +1,38 @@
+﻿namespace QubitExample {
+
+    open Microsoft.Quantum.Bitwise;
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Measurement;
+    
+
+    operation MeasureQubits(count : Int) : Int {
+
+        mutable resultsTotal = 0;
+
+        using (qubit = Qubit()) {
+            for (idx in 0..count) {
+
+                H(qubit);                
+                let result = MResetZ(qubit);
+                set resultsTotal += result == One ? 1 | 0;
+            }
+
+            return resultsTotal;
+        }
+    }
+
+    operation RandomNumberGenerator() : Bool[] {
+
+        mutable randomBits = new Bool[32];
+        
+        for (idx in 0..31) {
+            using(qubit = Qubit())  {   
+                H(qubit);                
+                let result = MResetZ(qubit);
+                set randomBits w/= idx <- result == One;
+            }
+        }
+        return randomBits;
+    }   
+}
